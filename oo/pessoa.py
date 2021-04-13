a760ef507a9f47d173ed199d73bca3f5bc0ceef8
Gabriel Paixão <gabrielpaixaoitp@gmail.com>
2021-04-13 02:26:12 -0300

    Criados atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,19 @@
 class Pessoa:
-    def __init__(self, nome = None, idade=35):
+    def __init__(self, *filhos, nome = None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
         return f'olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Gabriel'
-    print(p.nome)
-    print(p.idade)
+    gabriel = Pessoa(nome='Gabriel')
+    lucia = Pessoa(gabriel, nome='Lucia')
+    print(Pessoa.cumprimentar(lucia))
+    print(id(lucia))
+    print(lucia.cumprimentar())
+    print(lucia.nome)
+    print(lucia.idade)
+    for filho in lucia.filhos:
+        print(filho.nome)
